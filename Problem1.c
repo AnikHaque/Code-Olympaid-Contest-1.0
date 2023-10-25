348dea5ef218f69d5f1477035f563824f7fe23f8
Anik Haque <79439078+AnikHaque@users.noreply.github.com>
2023-10-25 20:21:15 +0600

    Update Problem1.c

--- Problem1.c
+++ Problem1.c
@@ -1,1 +1,28 @@
 
+#include <stdio.h>
+
+#define MOD 1000000007
+
+long long fib(int n) {
+    if (n <= 1)
+        return n;
+
+    long long a = 0, b = 1, c;
+    for (int i = 2; i <= n; i++) {
+        c = (a + b) % MOD;
+        a = b;
+        b = c;
+    }
+    return b;
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    long long result = fib(n);
+    printf("%lld\n", result);
+
+    return 0;
+}
+
